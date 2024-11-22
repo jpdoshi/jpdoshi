ed5d72531e4e33b2adb854eea5e9f075d1c89624
jpdoshi <jpdoshi2811@gmail.com>
2024-11-22 11:05:40 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold">jpdoshi.</h1>
+      <img className="m-4 h-[64px]" src="/pfp.png" alt="profile picture" />
     </main>
   );
 };
